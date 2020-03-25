1e5810d9ff5c97de5d8355403e2b79b5c02286d8
Felipe Gutierrez <felipeg@Felipes-MacBook-Pro.local>
2020-03-25 17:43:36 -0600

    updated

---
+++ ch06/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -N -P ./http-log/ https://repo.spring.io/release/org/springframework/cloud/stream/app/http-source-rabbit/2.1.1.RELEASE/http-source-rabbit-2.1.1.RELEASE.jar
+wget -N -P ./http-log/ https://repo.spring.io/release/org/springframework/cloud/stream/app/log-sink-rabbit/2.1.2.RELEASE/log-sink-rabbit-2.1.2.RELEASE.jar
+
