8fb1822e8b804dc9651019faba44a9e26a7247ac
Felipe Gutierrez <felipeg@ibm.com>
2020-03-31 17:42:35 -0600

    updated

--- ch06/function-bridge/movie-filter-kafka/src/main/java/com/apress/cloud/stream/movie/MovieStream.java
+++ ch06/function-bridge/movie-filter-kafka/src/main/java/com/apress/cloud/stream/movie/MovieStream.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Flux;
 
 import java.util.function.Function;
 
-/*
+
 @Configuration
 public class MovieStream {
     String GENRE_SCIENCE_FICTION = "science-fiction";
@@ -16,11 +16,11 @@ public class MovieStream {
         return flux -> flux.filter( movie -> movie.getGenre().equals(GENRE_SCIENCE_FICTION));
     }
 }
-*/
+
 
 //Using Function Composition
 //You need to enable the spring.cloud.function.definition property to have one or more functions to create a composition
-
+/*
 @Configuration
 public class MovieStream {
     String GENRE_SCIENCE_FICTION = "science-fiction";
@@ -38,3 +38,4 @@ public class MovieStream {
         });
     }
 }
+*/
