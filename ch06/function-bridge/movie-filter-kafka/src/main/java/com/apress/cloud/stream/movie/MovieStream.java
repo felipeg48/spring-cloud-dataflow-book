c119139ea6fddc4c55efcb27f48ef0af1809e699
Felipe Gutierrez <felipeg@ibm.com>
2020-03-31 16:56:17 -0600

    updated

--- ch06/function-bridge/movie-filter-kafka/src/main/java/com/apress/cloud/stream/movie/MovieStream.java
+++ ch06/function-bridge/movie-filter-kafka/src/main/java/com/apress/cloud/stream/movie/MovieStream.java
@@ -14,5 +14,27 @@ public class MovieStream {
     public Function<Flux<Movie>, Flux<Movie>> onlyScienceFiction() {
         return flux -> flux.filter( movie -> movie.getGenre().equals(GENRE_SCIENCE_FICTION));
     }
-
 }
+
+
+//Using Function Composition
+//You need to enable the spring.cloud.function.definition property to have one or more functions to create a composition
+/*
+@Configuration
+public class MovieStream {
+    String GENRE_SCIENCE_FICTION = "science-fiction";
+
+    @Bean
+    public Function<Flux<Movie>, Flux<Movie>> onlyScienceFiction() {
+        return flux -> flux.filter( movie -> movie.getGenre().equals(GENRE_SCIENCE_FICTION));
+    }
+
+    @Bean
+    public Function<Flux<Movie>, Flux<Movie>> titleUpperCase() {
+        return flux -> flux.map( movie -> {
+            movie.setTitle(movie.getTitle().toUpperCase());
+            return movie;
+        });
+    }
+}
+*/
--- ch06/function-bridge/movie-web-bridge-kafka/src/main/java/com/apress/cloud/stream/movie/MovieController.java
+++ ch06/function-bridge/movie-web-bridge-kafka/src/main/java/com/apress/cloud/stream/movie/MovieController.java
@@ -2,13 +2,14 @@ package com.apress.cloud.stream.movie;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+// Using StreamBridge helper class
 
 @Log4j2
 @AllArgsConstructor
@@ -19,7 +20,7 @@ public class MovieController {
 
     @PostMapping("/movies")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void toMovieSupplier(@RequestBody Movie movie) {
+    public void toMovieBinding(@RequestBody Movie movie) {
         log.info("Sending: {} ", movie);
         assert streamBridge != null;
 
@@ -27,6 +28,28 @@ public class MovieController {
         // This will create the 'movie-out-0' binding
         streamBridge.send("movie-out-0", movie);
     }
-
-
 }
+
+
+
+// Using Reactive API
+/*
+@RestController
+public class MovieController{
+    EmitterProcessor<Movie> processor = EmitterProcessor.create();
+
+    @PostMapping("/movies")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void toMovieSupplier(@RequestBody Movie movie) {
+        processor.onNext(movie);
+    }
+
+    @Configuration
+    class MovieSupplier {
+        @Bean
+        public Supplier<Flux<Movie>> movie() {
+            return () -> processor;
+        }
+    }
+}
+*/
