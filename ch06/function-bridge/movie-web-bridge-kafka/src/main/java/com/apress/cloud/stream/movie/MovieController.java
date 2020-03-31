311f0321831958f9b9e77544464e35137bcb2be4
Felipe Gutierrez <felipeg@ibm.com>
2020-03-31 15:40:36 -0600

    updated

--- ch06/function-bridge/movie-web-bridge-kafka/src/main/java/com/apress/cloud/stream/movie/MovieController.java
+++ ch06/function-bridge/movie-web-bridge-kafka/src/main/java/com/apress/cloud/stream/movie/MovieController.java
@@ -1,7 +1,6 @@
 package com.apress.cloud.stream.movie;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
@@ -12,10 +11,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
+@AllArgsConstructor
 @RestController
 public class MovieController {
 
-    @Autowired
     private StreamBridge streamBridge;
 
     @PostMapping("/movies")
@@ -23,6 +22,9 @@ public class MovieController {
     public void toMovieSupplier(@RequestBody Movie movie) {
         log.info("Sending: {} ", movie);
         assert streamBridge != null;
+
+        // You need to set the property: spring.cloud.stream.source=movie
+        // This will create the 'movie-out-0' binding
         streamBridge.send("movie-out-0", movie);
     }
 
