a4cb6e08d54f5cfd999e8ffaa8e8bf376fbcee5f
Felipe Gutierrez <felipeg@ibm.com>
2020-03-31 18:13:31 -0600

    updated

--- ch06/function-bridge/movie-filter-kafka/src/main/java/com/apress/cloud/stream/movie/MovieStream.java
+++ ch06/function-bridge/movie-filter-kafka/src/main/java/com/apress/cloud/stream/movie/MovieStream.java
@@ -1,8 +1,14 @@
 package com.apress.cloud.stream.movie;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
 
 import java.util.function.Function;
 
@@ -36,6 +42,26 @@ public class MovieStream {
             movie.setTitle(movie.getTitle().toUpperCase());
             return movie;
         });
+    }
+}
+*/
+
+
+//Example with multiple input arguments
+/*
+@Configuration
+public class MovieStream {
+    String GENRE_SCIENCE_FICTION = "science-fiction";
+
+    @Bean
+    public Function<Tuple2<Flux<Integer>,Flux<Movie>>, Flux<Message<Movie>>> movieTuple() {
+        return tuple -> {
+            Flux<Integer> integerFlux = tuple.getT1();
+            Flux<Movie> movieFlux = tuple.getT2();
+
+            return Flux.just(
+                    MessageBuilder.withPayload(movieFlux.blockFirst()).setHeader("stars",integerFlux.map(m -> m.toString())).build());
+        };
     }
 }
 */
