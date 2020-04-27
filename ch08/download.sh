dc010ef39de5483f2780facf7e5ae254413e6620
Felipe Gutierrez <felipeg@ibm.com>
2020-04-27 15:45:31 -0600

    updated

---
+++ ch08/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://repo.spring.io/release/org/springframework/cloud/spring-cloud-dataflow-server/2.4.2.RELEASE/spring-cloud-dataflow-server-2.4.2.RELEASE.jar
+wget https://repo.spring.io/release/org/springframework/cloud/spring-cloud-dataflow-shell/2.4.2.RELEASE/spring-cloud-dataflow-shell-2.4.2.RELEASE.jar
+wget https://repo.spring.io/release/org/springframework/cloud/spring-cloud-skipper-server/2.3.2.RELEASE/spring-cloud-skipper-server-2.3.2.RELEASE.jar
+
